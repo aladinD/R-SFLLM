6667e69838d467623abdb0ccd87096f17915881c
Aladin-Djuhera <aladin.djuhera@ibm.com>
2023-07-24 14:03:36 +0200

    Added SFL scripts

---
+++ scripts/aggregator.py
@@ -0,0 +1,91 @@
+import torch
+from torch.nn.parameter import Parameter
+from transformers import BertForSequenceClassification
+from typing import Dict, List
+
+
+class Aggregator:
+   """
+   Aggregator class for prototyping SFL. Handles the aggregation of client gradients.
+   """
+   def __init__(self, name: str) -> None:
+        self.name = name
+
+      
+   def accumulate_attentions(self, client_models: List[BertForSequenceClassification]) -> List[Dict]:
+    """
+    Accumulates/Collects the attentions of all clients and returns a List of 
+    Dicts including the parameter names and values as a key-value pair.
+    """
+    attentions = []
+
+    for model in client_models:
+        attention_gradients = {}
+        for name, param in model.named_parameters():
+            if name.startswith("bert.encoder"):
+                attention_gradients[name] = param
+
+        attentions.append(attention_gradients)
+
+    return attentions
+   
+   
+   def accumulate_heads(self, client_models: List[BertForSequenceClassification]) -> List[Dict]:
+    """
+    Accumulates/Collects the heads (classifier and pooler) of all clients and 
+    returns a List of Dicts including the parameter names and values as a 
+    key-value pair.
+    """
+    heads = []
+
+    for model in client_models:
+        head_gradients = {}
+        for name, param in model.named_parameters():
+            if name.startswith("classifier") or name.startswith("bert.pooler"):
+                head_gradients[name] = param
+
+        heads.append(head_gradients)
+
+    return heads
+
+
+   def accumulate_embeddings(self, client_models: List[BertForSequenceClassification]) -> List[Dict]:
+    """
+    Accumulates/Collects the embeddings of all clients and returns a List of 
+    Dicts including the parameter names and values as a key-value pair.
+    """
+    embeddings = []
+
+    for model in client_models:
+        embedding_gradients = {}
+        for name, param in model.named_parameters():
+            if name.startswith("bert.embeddings"):
+                embedding_gradients[name] = param
+
+        embeddings.append(embedding_gradients)
+
+    return embeddings
+
+   
+   def aggregate(self, client_gradients: List[Dict[str, Parameter]]) -> Dict[str, Parameter]:
+      """
+      Aggregates the parameters of the clients by simple averaging.
+      """
+      aggregated_gradients = {}
+      num_clients = len(client_gradients)
+
+      for client_gradient in client_gradients:
+         # Iterate over each parameter gradient in the client's gradients
+         for param_name, gradient in client_gradient.items():
+               if param_name not in aggregated_gradients:
+                  # Initialize the tensor to store the average
+                  aggregated_gradients[param_name] = torch.zeros_like(gradient.data)
+               
+               # Accumulate the gradient values across all clients
+               aggregated_gradients[param_name] += gradient.data
+
+      # Compute the average for each parameter gradient
+      for param_name in aggregated_gradients:
+         aggregated_gradients[param_name] /= num_clients
+
+      return aggregated_gradients
---
+++ scripts/client.py
@@ -0,0 +1,230 @@
+from manager import Manager
+import torch
+from torch.nn.parameter import Parameter
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import AdamW, get_linear_schedule_with_warmup, BertForSequenceClassification, BertTokenizer
+from typing import Dict, List, Tuple
+
+    
+class Client:
+    """
+    Client class for SFL.
+    """
+    def __init__(self, 
+                 name: str, 
+                 model: BertForSequenceClassification,
+                 manager: Manager, 
+                 train_data: DataLoader,
+                 valid_data: DataLoader) -> None:
+        self.name = name
+        self.model = model
+        self.manager = manager
+        self.train_data = train_data
+        self.valid_data = valid_data
+
+    
+    def save_model(self, path: str) -> None:
+        """
+        Saves the current model to the specified path.
+        """
+        torch.save(self.model.state_dict(), path)
+
+
+    def train_model(self, 
+                    device: torch.device, 
+                    num_epochs: int = 1, 
+                    learning_rate: float = 1e-5,
+                    epsilon: float = 1e-6,
+                    num_warmup_steps: float = 1256) -> Tuple[List[float], List[float]]:
+        """
+        Trains the client model on its data.
+        """
+        self.model.to(device)
+
+        total_steps = len(self.train_data) * num_epochs
+
+        optimizer = AdamW(self.model.parameters(), lr=learning_rate, eps=epsilon)
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=total_steps)
+
+        # Training Loop
+        global_loss = []
+        global_accuracy = []
+
+        for epoch in range(num_epochs):
+            print(f"Epoch {epoch + 1}/{num_epochs}")
+            print("-" * 10)
+
+            self.model.train()
+            
+            total_loss = 0
+            total_correct = 0
+            total_samples = 0
+            
+            # Training
+            for step, batch in enumerate(tqdm(self.train_data)):
+
+                batch = tuple(t.to(device) for t in batch)
+                inputs = {
+                    "input_ids": batch[0],
+                    "attention_mask": batch[1],
+                    "labels": batch[2]
+                }
+                
+                outputs = self.model(**inputs)
+                loss = outputs.loss
+                total_loss += loss.detach().float()
+
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+
+            # Validation
+            self.model.eval()
+
+            for step, batch in enumerate(tqdm(self.valid_data)):
+
+                batch = tuple(t.to(device) for t in batch)
+                inputs = {
+                    "input_ids": batch[0],
+                    "attention_mask": batch[1],
+                    "labels": batch[2]
+                }
+
+                with torch.no_grad():
+                    outputs = self.model(**inputs)
+
+                eval_loss = outputs.loss
+                logits = outputs.logits
+
+                predictions = torch.argmax(logits, dim=1)
+                true_labels = batch[2]
+
+                total_correct += (predictions == true_labels).sum().item()
+                total_samples += len(true_labels)
+                total_loss += eval_loss.item()
+
+            # Compute average loss and accuracy
+            average_loss = total_loss / len(self.train_data)
+            accuracy = total_correct / total_samples
+
+            print(f"Average Loss: {average_loss:.4f}")
+            print(f"Accuracy: {accuracy:.4f}")
+
+            global_loss.append(average_loss.cpu())
+            global_accuracy.append(accuracy)
+
+
+        # Save model
+        checkpoint_path = f"/home/munichcenter/code/bert/sst2/fedavg/results/bert_results/checkpoints/{self.name}_model.pt"
+        self.save_model(path=checkpoint_path)
+
+        print("Training complete!")
+
+        return global_loss, global_accuracy
+
+
+    def evaluate_model(self, 
+                       model_path: str, 
+                       device: torch.device, 
+                       valid_data: DataLoader) -> None:
+        """
+        Evaluates the client model on its validation data.
+        """
+        print("START EVALUATION")
+        self.valid_data = valid_data
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.to(device)
+        self.model.eval()
+
+        total_loss = 0
+        total_correct = 0
+        total_samples = 0
+
+        for step, batch in enumerate(tqdm(self.valid_data)):
+
+            batch = tuple(t.to(device) for t in batch)
+            inputs = {
+                "input_ids": batch[0],
+                "attention_mask": batch[1],
+                "labels": batch[2]
+            }
+
+            with torch.no_grad():
+                outputs = self.model(**inputs)
+
+            eval_loss = outputs.loss
+            logits = outputs.logits
+
+            predictions = torch.argmax(logits, dim=1)
+            true_labels = batch[2]
+
+            total_correct += (predictions == true_labels).sum().item()
+            total_samples += len(true_labels)
+            total_loss += eval_loss.item()
+
+        # Compute average loss and accuracy
+        average_loss = total_loss / len(self.valid_data)
+        accuracy = total_correct / total_samples
+
+        print(f"Average Loss: {average_loss:.4f}")
+        print(f"Accuracy: {accuracy:.4f}")
+
+
+    def evaluate_global_model(self, 
+                              device: torch.device, 
+                              valid_data: DataLoader) -> Tuple[float, float]:
+        """
+        Evaluates a global model (e.g. fedavg-ed) on its validation data and 
+        returns the average loss and accuracy.
+        """
+        print("START EVALUATION")
+        self.valid_data = valid_data
+        self.model.to(device)
+        self.model.eval()
+
+        total_loss = 0
+        total_correct = 0
+        total_samples = 0
+
+        for step, batch in enumerate(tqdm(self.valid_data)):
+
+            batch = tuple(t.to(device) for t in batch)
+            inputs = {
+                "input_ids": batch[0],
+                "attention_mask": batch[1],
+                "labels": batch[2]
+            }
+
+            with torch.no_grad():
+                outputs = self.model(**inputs)
+
+            eval_loss = outputs.loss
+            logits = outputs.logits
+
+            predictions = torch.argmax(logits, dim=1)
+            true_labels = batch[2]
+
+            total_correct += (predictions == true_labels).sum().item()
+            total_samples += len(true_labels)
+            total_loss += eval_loss.item()
+
+        # Compute average loss and accuracy
+        average_loss = total_loss / len(self.valid_data)
+        accuracy = total_correct / total_samples
+
+        print(f"Average Loss: {average_loss:.4f}")
+        print(f"Accuracy: {accuracy:.4f}")
+
+        return average_loss, accuracy
+
+
+    def update_model(self, updates: Dict[str, Parameter]) -> None:
+        """
+        Updates the client model with the aggregated weight updates.
+        """
+        for gradient_name in updates.keys():
+            for name, param in self.model.named_parameters():
+                if name == gradient_name:
+                    param.data = updates[gradient_name]
