7d0caee8e0007349fca22ec45a2f9d19a3399db9
Aladin-Djuhera <aladin.djuhera@ibm.com>
2023-07-26 17:08:18 +0200

    updated scripts

--- src/test.py
+++ src/test.py
@@ -6,7 +6,6 @@ from datamodules.bert_datamodule import BertDataModule
 def main():
     model = CustomBertModel.from_pretrained("bert-base-uncased", num_labels=2)
     datamodule = BertDataModule(glue_dataset="sst2", batch_size=32, num_workers=1, truncate=100)
-    # train_data, val_data = datamodule.train_dataloader(), datamodule.val_dataloader()
     trainer = pl.Trainer()
     trainer.fit(model, datamodule= datamodule)
 
