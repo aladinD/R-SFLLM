6f84ca909d273ef6ef8cf4820c3e496c37dfdad4
Aladin Djuhera <aladin.djuhera@tum.de>
2023-09-11 12:46:37 +0200

    disabled automatic model checkpoints in logger

--- src/train_sc.py
+++ src/train_sc.py
@@ -16,6 +16,8 @@ from src.models.roberta_module import RoBERTaForSequenceClassificationModule
 from src.utils import plotting
 from src.utils.utils import init_dir
 
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 
 # Seeding
 seed = 42
@@ -32,7 +34,11 @@ def parallel_train(client, cfg, r):
     Train and save a client model seperately in a parallel job.
     """
     logger = pl.loggers.CSVLogger(save_dir=cfg.training.client_log_path, name=f"client_{client.id}_logger", version=f"round_{r}")
-    client.trainer = pl.Trainer(**cfg.trainer, devices=[client.id], logger=logger, log_every_n_steps=1)
+
+    # Disable checkpoints logging 
+    checkpoint_callback = ModelCheckpoint(save_top_k=0, every_n_epochs=int(1e9))
+    
+    client.trainer = pl.Trainer(**cfg.trainer, devices=[client.id], logger=logger, log_every_n_steps=1, callbacks=[checkpoint_callback])
     client.trainer.fit(client.model, client.train_data, client.val_data)
     torch.save(client.model.state_dict(), cfg.training.client_ckpts_path + f"client_{client.id}.pt")
 
